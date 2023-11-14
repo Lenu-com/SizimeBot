7ba15a95671862aba490eb903b0bd487ff959a24
Lenu-com <oretyan@icloud.com>
2023-11-15 00:42:01 +0900

    first commit

---
+++ app/infrastructure/DiscordServerConnector.py
@@ -0,0 +1,19 @@
+from typing import Final
+import discord
+import os
+
+TOKEN: Final[str] = os.environ['DISCORD_API_TOKEN']
+
+class Client(discord.Client):
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
+        if self.user.mentioned_in(message):
+            await message.channel.send(message.content)
+
+intents = discord.Intents.default()
+intents.messages = True
+intents.message_content = True
+client = Client(intents=intents)
+client.run(TOKEN)
