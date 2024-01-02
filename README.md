81191929aed9e216efaf1eda052074ea748baf8c
Lenu-com <oretyan@icloud.com>
2024-01-03 00:58:59 +0900

    README.mdの追加

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# chatGPT×Discord 概要
+当プログラムはDiscordAPIとOpenAIAPIを使用し、<br>
+Discord内でのBOTへのメッセージをプロンプトとして<br>
+chatGPTへリクエストを送信し、レスポンスをBOTが送信する事で<br>
+Discord内でのchatGPTの使用を可能にします。
+
+# 使用方法
+1. app.pyにDiscord Botのトークンを設定します。<br>
+   ```
+   TOKEN: Final[str] = 'YOUR_DISCORD_BOT_TOKEN'
+   ```
+
+2. OpenAIConnector.pyにOpenAIのAPIキーを設定します。<br>
+    ```
+    API_KEY: Final[str] = 'YOUR_OPENAI_API_KEY'
+    ```
+
+3. config.ymlに事前情報を設定します。
+    ```
+    content:
+        ここにキャラ設定や制約などを記載する。
+    
+    ng_words:
+        - ここに禁止ワードを記載する。(頭に- を付ける。)
+    ```
+
+4. app.pyを実行し、本ツールを起動します。
+5. BOT宛にメンションを付け、メッセージを送信します。
+
+# テストサーバー公開
+※一時的な公開であり、予告なしにテスト終了する場合があります。<br>
+https://discord.gg/Fm8PNwP6
+
+
--- app/OpenAIConnector.py
+++ app/OpenAIConnector.py
@@ -7,12 +7,13 @@ def load_config():
     file_path = 'config.yml'
     with open(file_path, 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
-    return config['config']
+    return config
 
 MODEL: Final[str] = 'gpt-3.5-turbo'
 TOKEN_MAX: Final[int] = 4096
 API_KEY: Final[str] = 'YOUR_OPENAI_API_KEY'
-CONFIG: Final[str] = load_config()
+CONFIG: Final[str] = load_config()['config']
+NG_WORDS: Final[list[str]] = load_config()['ng_words']
 
 def is_token_over(message: str):
     encode = tiktoken.encoding_for_model(MODEL)
@@ -21,6 +22,8 @@ def is_token_over(message: str):
     total_token = len(config_token) + len(message_token)
     return total_token > TOKEN_MAX
 
+def is_ng_word(message: str):
+    return any(ng_word in message for ng_word in NG_WORDS)
 
 def get_message(message: str):
     client = OpenAI(
@@ -28,7 +31,10 @@ def get_message(message: str):
     )
     
     if is_token_over(message):
-        message = 'メッセージが長すぎます。とUserに送信してください。'
+        message = 'メッセージが長すぎる旨をUserに伝えてください。'
+        
+    if is_ng_word(message):
+        message = 'NGワードが含まれている為、返答できない旨をUserに伝えてください。'
     
     chat_completion = client.chat.completions.create(
         messages=[
